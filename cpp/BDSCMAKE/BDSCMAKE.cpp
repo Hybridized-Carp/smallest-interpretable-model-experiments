efa6f71ea057dbb74ec99a73bc28927ae7576449
Hybridized-Carp <294808108+Hybridized-Carp@users.noreply.github.com>
2026-07-24 17:36:18 +0100

    reset to state on 250226

--- cpp/BDSCMAKE/BDSCMAKE.cpp
+++ cpp/BDSCMAKE/BDSCMAKE.cpp
@@ -4,7 +4,6 @@
 #include "BDSCMAKE.h"
 #include "nlohmann/json.hpp"
 
-
 #include <stdio.h>
 #include <iostream>
 #include <sstream>
@@ -19,107 +18,38 @@ using namespace std;
 
 using json = nlohmann::json;
 
-//this is kinda arbitrary because I could invent arbitrary coding schemes
-enum binaryEncoding{
-    OneHot = 1,
-    Label = 2,
-    Ordinal = 3,
-    Unary = 4
-};
-
 namespace parser {
-    struct feature {
-        string name;
+    enum encoding {
+        OneHot = 1,
+        Label = 2,
+
+    };
+    struct symbol {
+        string identifier;
+        vector<char> mapping;
+    };
+    struct column {
+        vector<symbol> symbols;
         bool target;
-        map<string,uint8_t> symbols;
-        binaryEncoding encodingType;
-    };
-
-    void to_json(json& j, const feature& f) {
-        j = json{ {"name", f.name}, {"target", f.target}, {"symbols", f.symbols}, {"encoding", f.encodingType}};
-    }
-
-    void from_json(const json& j, feature& f) {
-        j.at("name").get_to(f.name);
-        j.at("target").get_to(f.target);
-        j.at("symbols").get_to(f.symbols);
-        j.at("encoding").get_to(f.encodingType);
-    }
-}
-
-namespace genericClassifier {
-    struct feature {
-        string name;
-        uint8_t value;
-        uint8_t size;
-        binaryEncoding encodingType;
-    };
-    struct datapoint {
-        vector<feature> data;
-        bool result;
-    };
-    struct genericClassificationInstance{
-        vector<datapoint> datapoints;
-        vector<parser::feature> features;
+        string Title;
     };
 }
 
-namespace binaryClassification {
-    struct datapoint {
-        vector<bool> data;
-        bool result;
-    };
-
-    struct binaryClassificationInstance {
-        vector<datapoint> examples;
-        vector<genericClassifier::feature> features;
-    };
-
-    binaryClassificationInstance from_genericClassificationInstance(genericClassifier::genericClassificationInstance genericClassifierInstance) {
-        vector<datapoint> bdatapoints;
-        for (genericClassifier::datapoint orgdatapoint: genericClassifierInstance.datapoints) {
-            vector<bool> data;
-            for (genericClassifier::feature feature: orgdatapoint.data)
-                    switch (feature.encodingType) {
-                    case (binaryEncoding::OneHot):
-                        for (int j = 0; j < feature.size; j++)
-                            data.push_back(feature.value == j);
-                        break;
-                    case (binaryEncoding::Label):
-                        for (int j = 1; j < feature.size; j++)
-                            data.push_back(feature.value == j);
-                        break;
-                    case (binaryEncoding::Ordinal):
-                        //can't be fucked rn
-                        break;
-                    case(binaryEncoding::Unary):
-                        for (int j = 1; j < feature.size; j++)
-                            data.push_back(feature.value >= j);
-                        break;
-                    }
-            
-            bdatapoints.push_back({ data, orgdatapoint.result});
-        }
-        return { bdatapoints , genericClassifierInstance.datapoints[0].data};
-    }
-}
 
 int main()
 {
     string Filename = "agaricus-lepiota.data";
+    string schemaFilename = Filename + "_schema.json";
     cout << "Opening " << Filename << endl;
-    ifstream rawDataset(Filename);
+    ifstream rawDataset("agaricus-lepiota.data");
 
-    if (!rawDataset.is_open()) { 
-        cout << "Failed to Open " << Filename << endl; 
-        return -1; 
-    }
+    if (!rawDataset.is_open()) { return -1; }
 
     vector<string> lines;
     string line;
     int items = -1;
 
-    cout << "Reading lines" << endl;
+    cout << "reading lines" << endl;
     //read in lines
     while (getline(rawDataset, line)) {
         if (!line.empty()) {
@@ -137,7 +67,7 @@ int main()
     //add one to items because we actually count the number of comma's
     items++;
 
-    cout << "Splitting lines into row elements" << endl;
+    cout << "turning lines into rows" << endl;
     vector<vector<string>> rows;
     for (string line : lines) {
         vector<string> row;
@@ -149,122 +79,49 @@ int main()
         rows.push_back(row);
     }
 
-    string schemaFilename = Filename + "_schema.json";
-    cout << "Checking for "<< schemaFilename << endl;
-    vector<parser::feature> features;
-    if (!filesystem::exists(schemaFilename))
-    {
-        cout << "Schema not found, generating template schema" << endl;
-        cout << "Finding unique items in columns" << endl;
+    cout << "finding unique items in column" << endl;
 
-        vector<set<string>> unique_items_in_column;
-        for (int i = 0; i < items; i++) {
-            set<string> column;
-            for (vector<string> row : rows) {
-                column.insert(row[i]);
-            }
-            unique_items_in_column.push_back(column);
+    vector<set<string>> unique_items_in_column;
+    for (int i = 0; i < items; i++) {
+        set<string> column;
+        for (vector<string> row : rows) {
+            column.insert(row[i]);
         }
+        unique_items_in_column.push_back(column);
+    }
 
-        cout << "Creating feature list" << endl;
-        for (set<string> unique_items : unique_items_in_column) {
-            //use category toggle if we only have one item
-            //might be incorrect but oh well
-            uint8_t fi = 0;
-            map<string, uint8_t> symbols;
-            for (string unique_item : unique_items) {
-                symbols.insert({ unique_item, fi });
-                fi++;
-            }
-            features.push_back(
-                parser::feature(
-                    "unknown",
-                    false,
-                    symbols,
-                    unique_items.size() != 2 ? binaryEncoding::OneHot : binaryEncoding::Label
-                )
-            );
+    cout << "creating feature maps" << endl;
+    vector<map<string, int>> categorymaps;
+    for (set<string> unique_items : unique_items_in_column) {
+        //use category toggle if we only have one item
+        //might be incorrect but oh well
+        map<string, int> categorymap;
+        int fm = 0;
+        for (string unique_item : unique_items) {
+            categorymap.insert({ unique_item, fm });
+            fm++;
         }
-        cout << "Writing "<< schemaFilename << endl;
-        json j = features;
-        ofstream schemaStream(schemaFilename);
-        schemaStream << j.dump(4);
-        schemaStream.close();
-        cout << "Wrote " << schemaFilename << endl;
-        cout << "Please set a target feature and then rerun the program";
-        return -3;
+        categorymaps.push_back(categorymap);
     }
 
-    cout << "Reading " << schemaFilename << endl;
-    ifstream fileSchema(schemaFilename);
-
-    if (!fileSchema.is_open()) {
-        cout << "Failed to Open " << schemaFilename << endl;
-        return -4;
-    }
-    cout << "Parsing " << schemaFilename << endl;
-    json data = json::parse(fileSchema);
-    features = data.get<vector<parser::feature>>();
-    cout << "Closing " << schemaFilename << endl;
-    fileSchema.close();
-    cout << "Searching for target feature" << endl;
-    int targetFeatureIndex = -1;
-    for (int i = 0; i < features.size(); i++)
-    {
-        if (features[i].target && targetFeatureIndex ==-1 && features[i].symbols.size() == 2) {
-            targetFeatureIndex = i;
-        } else if (features[i].target) {
-            cout << "Too many target features found or target feature has more than 2 symbols";
-            return -5;
-        }
-    }
-    if (targetFeatureIndex == -1) {
-        cout << "Please set a target feature and then rerun the program";
-        return -6;
-    }
-    
-    //bit of testing code
-    int numbits = 0;
-    for (parser::feature feature : features)
-    {
-        if (feature.target)
-            continue;
-        switch (feature.encodingType) {
-            case (binaryEncoding::OneHot):
-                numbits += feature.symbols.size();
-                break;
-            case (binaryEncoding::Label):
-            case (binaryEncoding::Unary):
-                numbits += feature.symbols.size() - 1;
-                break;
-            case (binaryEncoding::Ordinal):
-                uint8_t size = feature.symbols.size();
-                int bits = 0;
-                while (size >>= 1)
-                    bits++;
-                numbits += bits;
-                break;
-        }
-        
-    }
-
-    cout << "applying feature maps to rows to create generic classifier instance" << endl;
-    vector<genericClassifier::datapoint> datapoints;
+    cout << "applying feature maps to rows to create boolean vectors" << endl;
+    vector<vector<bool>> datapoints;
     for (vector<string> row : rows) {
-        genericClassifier::datapoint datapoint;
+        vector<bool> datapoint;
         for (int i = 0; i < row.size(); i++) {
-            parser::feature feature = features[i];
-            if (feature.target)
-                datapoint.result = (bool)feature.symbols[row[i]];
+            map<string, int> categorymap = categorymaps[i];
+            string feature = row[i];
+            if (categorymap.size() == 2)
+                datapoint.push_back(categorymap[feature] == 1);
             else {
-                genericClassifier::feature clfeature{ feature.name, feature.symbols[row[i]], feature.symbols.size(), feature.encodingType };
-                datapoint.data.push_back(clfeature);
+                for (int j = 0; j < categorymap.size(); j++)
+                {
+                    datapoint.push_back(categorymap[feature] == j);
+                }
             }
         }
         datapoints.push_back(datapoint);
     }
-    genericClassifier::genericClassificationInstance gci = {datapoints, features};
-    binaryClassification::binaryClassificationInstance bCi = binaryClassification::from_genericClassificationInstance(gci);
 	cout << "Hello CMake." << endl;
 	return 0;
 }
